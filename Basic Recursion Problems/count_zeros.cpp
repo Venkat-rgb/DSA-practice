b1e84258f99f8b318d1dc1ffc0b3da5d45f5c567
Venkat-rgb <venkymandadi62@gmail.com>
2024-09-17 22:52:18 +0530

    Count number of zeros recursion

---
+++ Basic Recursion Problems/count_zeros.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int countZeros(int n) {
+    if(n <= 9){
+        if(n == 0) return 1;
+        else return 0;
+    }
+
+    int res = countZeros(n / 10);
+
+    if(n % 10 == 0) {
+        return res + 1;
+    }
+    else {
+        return res;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << countZeros(n) << endl;
+}
+
+// Refer to coding ninjas assignment for recursion for this problem
