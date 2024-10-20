fc7c2eed5f5c046af73bb81882122eee6f291a47
Venkat-rgb <venkymandadi62@gmail.com>
2024-10-20 10:44:14 +0530

    Find floor and ceil in array

---
+++ Binary Search/Easy/Find floor and ceil in array.cpp
@@ -0,0 +1,60 @@
+#include <algorithm>
+
+int getCeil(int n, int x, vector<int> &arr)
+{
+    int ans = -1;
+
+    // Initial range of search
+    int l = 0, h = n - 1;
+    while (l <= h)
+    {
+
+        int mid = (l + h) / 2;
+
+        // Array element at Mid is greater than x.
+        if (arr[mid] >= x)
+        {
+            ans = arr[mid];
+
+            // We reduce search space to left half.
+            h = mid - 1;
+        }
+        else
+        {
+
+            // We reduce search space to right half.
+            l = mid + 1;
+        }
+    }
+    return ans;
+}
+int getFloor(int n, int x, vector<int> &arr)
+{
+    int ans = -1;
+
+    // Initial range of search
+    int l = 0, h = n - 1;
+    while (l <= h)
+    {
+
+        int mid = (l + h) / 2;
+
+        // Array element at Mid is less than x.
+        if (arr[mid] <= x)
+        {
+            ans = arr[mid];
+            // We reduce search space to right half.
+            l = mid + 1;
+        }
+        else
+        {
+            // We reduce search space to left half.
+            h = mid - 1;
+        }
+    }
+    return ans;
+}
+pair<int, int> getFloorAndCeil(vector<int> &a, int n, int x)
+{
+    return make_pair(getFloor(n, x, a), getCeil(n, x, a));
+}
