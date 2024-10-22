e2ce47af8a4311301794747cf4ef5ffbb1edab93
Venkat-rgb <venkymandadi62@gmail.com>
2024-10-22 13:54:21 +0530

    Count occurences of a number in a sorted array with duplicates

---
+++ Binary Search/Easy/Count occurrences of a number in a sorted array with duplicates.cpp
@@ -0,0 +1,9 @@
+int count(int arr[], int n, int x) {
+    auto lower = lower_bound(arr, arr + n, x);
+    auto upper = upper_bound(arr, arr + n, x);
+    
+    int firstOcc = lower - arr;
+    int lastOcc = upper - arr;
+    
+    return lastOcc - firstOcc;
+}
