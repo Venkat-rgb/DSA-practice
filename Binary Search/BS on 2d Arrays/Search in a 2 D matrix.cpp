affa233e69e88abe7552bf64cd609260ec033c6a
Venkat-rgb <venkymandadi62@gmail.com>
2024-11-09 12:54:12 +0530

    Search a 2D Matrix

---
+++ Binary Search/BS on 2d Arrays/Search in a 2 D matrix.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size(), n = matrix[0].size();
+
+        int start = 0, end = (m * n) - 1;
+
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            int row = mid / n, col = mid % n;
+
+            if(matrix[row][col] == target) {
+                return true;
+            }
+            else if(matrix[row][col] > target) {
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+
+        return false;
+    }
+};
