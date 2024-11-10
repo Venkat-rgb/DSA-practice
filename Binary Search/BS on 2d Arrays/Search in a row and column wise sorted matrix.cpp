22d5dae7c27b9578f40fc45d30ecc54a55b81453
Venkat-rgb <venkymandadi62@gmail.com>
2024-11-10 12:52:27 +0530

    Search in a row and column wise sorted matrix

---
+++ Binary Search/BS on 2d Arrays/Search in a row and column wise sorted matrix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size(), n = matrix[0].size();
+
+        int row = m - 1, col = 0;
+
+        while(row >= 0 && col < n) {
+            if(matrix[row][col] == target) {
+                return true;
+            }
+            else if(target > matrix[row][col]) {
+                col++;
+            }
+            else {
+                row--;
+            }
+        }
+
+        return false;
+    }
+};
