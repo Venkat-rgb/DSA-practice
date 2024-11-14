3bfa2e5a352513d6a826ac566e129d63bdd7be25
Venkat-rgb <venkymandadi62@gmail.com>
2024-11-14 10:13:52 +0530

    Largest Odd Number in String

---
+++ Strings/Easy/Largest odd number in a string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        int n = num.size();
+        
+        // If num is already odd
+        if((num.back() - '0') % 2 == 1) {
+            return num;
+        }
+
+        for(int i = n - 1; i >= 0; --i) {
+            int singleNum = num[i] - '0';
+            if(singleNum % 2 == 1) {
+                return num.substr(0, i + 1);
+            }
+        }
+        
+        return ""; 
+    }
+};
