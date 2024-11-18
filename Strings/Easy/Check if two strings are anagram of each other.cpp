1acdee5c29bc55989f12355e9bba881d68c9e28c
Venkat-rgb <venkymandadi62@gmail.com>
2024-11-18 15:36:58 +0530

    Check if two strings are anagram of each other

---
+++ Strings/Easy/Check if two strings are anagram of each other.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int arr[26] = {0};
+
+        if(s.size() != t.size()) {
+            return false;
+        }
+
+        for(int i = 0; i < s.size(); ++i) {
+            arr[s[i] - 'a']++;
+        }
+
+        for(int i = 0; i < t.size(); ++i) {
+            if(arr[t[i] - 'a'] > 0) {
+                arr[t[i] - 'a']--;
+            }
+        }
+
+        for(int i = 0; i < 26; ++i) {
+            if(arr[i] > 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
