9d633ac383d7d74fc397576658c4b2938529cb08
Venkat-rgb <venkymandadi62@gmail.com>
2024-11-20 20:59:34 +0530

    Maximum Nesting Depth of the Parentheses

---
+++ Strings/Medium/Maximum Nesting Depth of Paranthesis.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int n = s.size();
+        
+        if(n == 1) {
+            return 0;
+        }
+
+        int maxi = 0, cnt = 0;
+
+        for(int i = 0; i < n; ++i) {
+            if(s[i] == '(') {
+                cnt++;
+                maxi = max(maxi, cnt);
+            }
+            else if(s[i] == ')') {
+                cnt--;
+            }
+        }
+
+        return maxi;
+    }
+};
