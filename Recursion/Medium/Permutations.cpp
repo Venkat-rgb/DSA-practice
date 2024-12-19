0a6d40d3214aa594f46029d8b69c78ea1d83f786
Venkat-rgb <venkymandadi62@gmail.com>
2024-12-19 20:27:32 +0530

    Permutations

---
+++ Recursion/Medium/Permutations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void helper(vector <int> &nums, int start, vector<vector<int>> &res) {
+        // 1) Base case
+        if(start == nums.size()) {
+            res.push_back({});
+            return;
+        }
+
+        // 2) Recursion work
+        vector<vector<int>> temp;
+        helper(nums, start + 1, temp);
+
+        // 3) My work
+        for(int i = 0; i < temp.size(); ++i) {
+            for(int j = 0; j <= temp[i].size(); ++j) {
+                vector <int> ans = temp[i];
+                ans.insert(ans.begin() + j, nums[start]);
+                res.push_back(ans);
+            }
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector <vector<int>> res;
+        helper(nums, 0, res);
+        return res;
+    }
+};
