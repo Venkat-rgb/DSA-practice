21ea283e10f2da2926cfd08d6fd4b2f791abef6f
Venkat-rgb <venkymandadi62@gmail.com>
2025-02-24 11:31:07 +0530

    Find the starting point in LL

---
+++ Linked List/Medium/Find the starting point in LL.cpp
@@ -0,0 +1,23 @@
+ListNode* detectCycle(ListNode* head) {
+    if (head == NULL || head->next == NULL) {
+        return NULL;
+    }
+
+    ListNode *slow = head, *fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) {
+            slow = head;
+
+            while (slow != fast) {
+                slow = slow->next;
+                fast = fast->next;
+            }
+
+            return slow;
+        }
+    }
+    return NULL;
+}
