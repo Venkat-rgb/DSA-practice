b75cdf6ba1538281e3af98fef16fb7ef847a4b0a
Venkat-rgb <venkymandadi62@gmail.com>
2025-02-28 11:43:30 +0530

    Delete the middle node of LL

---
+++ Linked List/Medium/Delete the middle node of LL.cpp
@@ -0,0 +1,16 @@
+ListNode* deleteMiddle(ListNode* head) {
+    if (head == NULL || head -> next == NULL) {
+        return NULL;
+    }
+
+    ListNode *slow = head, *fast = head, *prev = NULL;
+
+    while (fast != NULL && fast->next != NULL) {
+        prev = slow;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    prev -> next = prev -> next -> next;
+    return head;
+}
