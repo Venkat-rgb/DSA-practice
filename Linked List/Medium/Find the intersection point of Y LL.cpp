b9e79d09f2b0230a1c9bf805e86dfe00bc7483dd
Venkat-rgb <venkymandadi62@gmail.com>
2025-03-02 20:21:50 +0530

    Find the intersection point of Y LL

---
+++ Linked List/Medium/Find the intersection point of Y LL.cpp
@@ -0,0 +1,27 @@
+ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+    if (headA == NULL || headB == NULL) {
+        return NULL;
+    }
+
+    ListNode *temp1 = headA, *temp2 = headB;
+
+    while(temp1 != NULL && temp2 != NULL) {
+        if(temp1 == temp2) {
+            return temp1;
+        }
+        temp1 = temp1 -> next;
+        temp2 = temp2 -> next;
+     
+        if(temp1 == NULL && temp2 == NULL) {
+            return NULL;
+        }
+        if(temp1 == NULL) {
+            temp1 = headB;
+        }
+        if(temp2 == NULL) {
+            temp2 = headA;
+        }
+    }
+
+    return NULL;
+}
