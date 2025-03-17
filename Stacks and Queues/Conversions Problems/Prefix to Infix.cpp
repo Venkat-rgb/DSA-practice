a1b679078ea47224673cf2c14e86fd61c8290d94
Venkat-rgb <venkymandadi62@gmail.com>
2025-03-17 10:40:19 +0530

    Prefix to infix

---
+++ Stacks and Queues/Conversions Problems/Prefix to Infix.cpp
@@ -0,0 +1,28 @@
+// User function Template for C++
+
+class Solution {
+    public:
+      string preToInfix(string pre_exp) {
+          stack <string> st;
+          int n = pre_exp.size();
+          
+          for(int i = n - 1; i >= 0; --i) {
+              if(isalnum(pre_exp[i])) {
+                  st.push(string(1, pre_exp[i]));
+              }
+              else {
+                  if(st.size() >= 2) {
+                      string s1 = st.top();
+                      st.pop();
+                      string s2 = st.top();
+                      st.pop();
+                      
+                      string res = "(" + s1 + pre_exp[i] + s2 + ")";
+                      st.push(res);
+                  }
+              }
+          }
+          
+          return st.top();
+      }
+  };
