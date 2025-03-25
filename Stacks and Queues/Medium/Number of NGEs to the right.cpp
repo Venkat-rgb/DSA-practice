97202719dd0e9b63d7985dd67705b95ac413f6b8
Venkat-rgb <venkymandadi62@gmail.com>
2025-03-25 11:02:29 +0530

    Number of NGEs to the right

---
+++ Stacks and Queues/Medium/Number of NGEs to the right.cpp
@@ -0,0 +1,22 @@
+//User function Template for C++
+
+class Solution{
+    public:
+    
+        vector<int> count_NGE(int n, vector<int> &arr, int queries, vector<int> &indices){
+            vector <int> res;
+            
+            for(int i = 0; i < queries; ++i) {
+                int cnt = 0;
+                for(int j = indices[i] + 1; j < n; ++j) {
+                    if(arr[j] > arr[indices[i]]) {
+                        cnt++;
+                    }
+                }
+                res.push_back(cnt);
+            }
+            
+            return res;
+        }
+    
+    };
