874018b27753be6cfdc9f97acbe3298530daf652
Venkat-rgb <venkymandadi62@gmail.com>
2025-05-03 15:51:20 +0530

    Print prime factors of a number

---
+++ Bit Manipulation/Print prime factors of a number.cpp
@@ -0,0 +1,15 @@
+vector<int> AllPrimeFactors(int N) {
+    vector <int> res;
+    
+    for(int i = 2; i * i <= N; ++i) {
+        if(N % i == 0) {
+            res.push_back(i);
+            while(N % i == 0) {
+                N = N / i;
+            }
+        }
+    }
+    
+    if(N != 1) res.push_back(N);
+    return res;
+}
