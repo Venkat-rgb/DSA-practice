7d9de3f5650ddc2cea3410b029969d9609c452ef
Venkat-rgb <venkymandadi62@gmail.com>
2025-05-04 16:53:37 +0530

    Sieve of Erastothenes

---
+++ Bit Manipulation/Sieve of Erastothenes.cpp
@@ -0,0 +1,17 @@
+void countPrimes(int n) {
+    vector <int> arr(n, 1);
+
+    for(int i = 2; i * i < n; ++i) {
+        if(arr[i] == 1) {
+            for(int j = i * i; j < n; j += i) {
+                arr[j] = 0;
+            } 
+        }
+    }
+
+    for(int i = 2; i < n; ++i) {
+        if(arr[i] == 1) {
+            cout << i << " ";
+        }
+    }
+}
