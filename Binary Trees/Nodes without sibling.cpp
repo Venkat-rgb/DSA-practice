e0e902890767b5d668901dbe2b0843a1079d7242
Venkat-rgb <venkymandadi62@gmail.com>
2025-06-14 20:57:58 +0530

    Nodes without sibling

---
+++ Binary Trees/Nodes without sibling.cpp
@@ -0,0 +1,13 @@
+void printNodesWithoutSibling(BinaryTreeNode<int> *root) {
+    if(root == NULL || (root -> left == NULL && root -> right == NULL)) return;
+
+	if(root -> left != NULL && root -> right == NULL) {
+		cout << root -> left -> data << " ";
+	}
+	if(root -> right != NULL && root -> left == NULL) {
+		cout << root -> right -> data << " ";
+	}
+
+	printNodesWithoutSibling(root -> left);
+	printNodesWithoutSibling(root -> right);
+}
