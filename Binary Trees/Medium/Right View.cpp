7bdedd03a20f037c53f3fb15538e713318c2c505
Venkat-rgb <venkymandadi62@gmail.com>
2025-07-04 06:58:12 +0530

    Right View

---
+++ Binary Trees/Medium/Right View.cpp
@@ -0,0 +1,16 @@
+void dfs(TreeNode *root, int level, vector <int> &res) {
+        if(root == NULL) return;
+
+        if(level == res.size()) {
+            res.push_back(root -> val);
+        }
+
+        dfs(root -> right, level + 1, res);
+        dfs(root -> left, level + 1, res);
+    }
+
+    vector<int> rightSideView(TreeNode* root) {
+        vector <int> res;
+        dfs(root, 0, res);
+        return res;
+    }
